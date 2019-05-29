b4b30b05db85d894a847ab10ddf5dc7239ec1883
PotapenkoAS <PotapenkoAC@gmail.com>
2019-05-29 19:41:51 +0300

    dates fix in trainer schedule

--- src/main/resources/static/scripts/new_train.js
+++ src/main/resources/static/scripts/new_train.js
@@ -16,8 +16,13 @@ function refreshSchedule(offset) {
             var dayCounter = 0;
             var arr = [];
             for (var i = 0; i < data.length; i++) {
-                arr[++j] = '<tr><td>';
-                arr[++j] = '<a href=""><p class="tooltip"><span class="tooltiptext"></span></p></a>'
+                arr[++j] = '<tr>';
+                if (data[i].days.MONDAY.count > 0){
+                    arr[++j] = '<td style="background-color: aqua">';
+                    arr[++j] = '<a href=""><p class="tooltip">' +data[i].days.MONDAY.count+' занятий<span class="tooltiptext">' + 'data[i].days.MONDAY.text' + '</span></p></a>'
+                    arr[++j] = '<td/><td>';
+                }
+
             }
         }
     })
